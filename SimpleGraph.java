042d35ea0fe4970d6c1b41031cc7e34c4b7e4ee3
dkreutter <63475753+dkreutter@users.noreply.github.com>
2021-01-21 16:19:09 -0500

    Update SimpleGraph.java
    Whitespace fixes, documentation added, bugfixes

--- SimpleGraph.java
+++ SimpleGraph.java
@@ -3,57 +3,61 @@ import java.util.Arrays;
 
 public class SimpleGraph extends Graph {
 	
-	public SimpleGraph() {
+	public SimpleGraph() {	//Constructs the graph with a single vertex
 		V = new int[1];
 		V[0] = 0;
 		E = new ArrayList<int[]>();
 	}
 	
-	public SimpleGraph(int numvert) {
-		V = new int[numvert];
+	public SimpleGraph(int numVert) {  //Constructs the graph with numVert vertices and no edges
+		V = new int[numVert];
+		
 		for(int i = 0; i < V.length; i++) {
 			V[i] = i;
 		}
+		
 		E = new ArrayList<int[]>();
 	}
 	
-	private SimpleGraph(int numVert, ArrayList<int[]> edges) {
+	private SimpleGraph(int numVert, ArrayList<int[]> edges) {  //Constructs the graph with numVert vertices and the edge set defined by edges.
 		V = new int[numVert];
 		E = new ArrayList<int[]>();
+		
 		for(int i = 0; i < numVert; i++) 
 			V[i] = i;
 		
-		for(int i = 0; i < edges.size(); i++) {
+		for(int i = 0; i < edges.size(); i++) {  //If an edge of edges has length greater than 2, constructs the graph with no edges.
 			if(edges.get(i).length != 2) {
 				System.out.println("Invalid Edge Set");
 				E.clear();
 				return;
 			}
-			for(int j = 0; j < edges.get(i).length; j++) {
+			
+			for(int j = 0; j < edges.get(i).length; j++) {  //If edges contains an edge with a vertex not in V, constructs the graph with no edges.
 				if (edges.get(i)[j] > numVert) {
 					System.out.println("Invalid Edge Set");
 					E.clear();
 					return;
 				}
 			}
-			E.add(edges.get(i));
 			
-		}
-		
+			E.add(edges.get(i));	
+		}	
 	}
 	
-	private SimpleGraph(int [] v, ArrayList<int[]> e) {
+	private SimpleGraph(int[] v, ArrayList<int[]> e) {
 		V = v;
 		E = e;
 	}
 	
-	public static SimpleGraph K(int numVert) {
+	public static SimpleGraph K(int numVert) { //Returns the complete graph on numVert vertices
 		int[] v = new int[numVert];
 		
 		for(int i = 0; i < numVert; i++) 
 			v[i] = i;
 		
 		ArrayList<int[]> edges = new ArrayList<int[]>();
+		
 		for(int i = 0; i < numVert; i++) {
 			for(int j = 0; j < numVert; j++) {
 				int[] edge = {i,j};
@@ -65,9 +69,10 @@ public class SimpleGraph extends Graph {
 		return G;
 	}
 	
-	public static SimpleGraph K(int a, int b) { 
+	public static SimpleGraph K(int a, int b) {  //Returns the complete bipartite graph with partite set sizes a, b
 		
 		ArrayList<int[]> edges = new ArrayList<int[]>();
+		
 		for(int i = 0; i < a; i++) {
 			for(int j = a; j < a+b; j++) {
 				int[] e = {i,j};
@@ -77,20 +82,20 @@ public class SimpleGraph extends Graph {
 		
 		SimpleGraph G = new SimpleGraph(a+b, edges);
 		return G;
-		
 		}
 	
 	public String toString() {
 		String v = Arrays.toString(this.V);
 		StringBuilder sb = new StringBuilder();
+		
 		for (int[] e : this.E)
 		{
 		    sb.append(Arrays.toString(e));
 		    sb.append(", ");
 		}
+		
 		String s = sb.toString();
 		return "(" + v + ", {" + s.substring(0, s.length()-2) + "})";
-	
 	}
 	
 	public SimpleGraph inducedSubgraph(int[] v) {
@@ -101,27 +106,30 @@ public class SimpleGraph extends Graph {
 			if (in(v, this.E.get(i)[0]) && in(v, this.E.get(i)[1]))
 				edges.add(E.get(i));
 		}
+		
 		return new SimpleGraph(v, edges);
 	}
 	
-	public boolean remove(int v) {
+	public boolean remove(int v) {  //Removes a vertex v and all incident edges from this. Returns false if v is not in this.V
 		if(!in(this.V, v))
 			return false;
 		
 		int[] newV = new int[this.V.length-1];
 		int j = 0;
+		
 		for(int i = 0; i < this.V.length; i++) {
 			if (!(this.V[i] == v)) {
 				newV[j] = this.V[i];
 				j++;
 			}
 		}
+		
 		this.V = this.inducedSubgraph(newV).V;
 		this.E = this.inducedSubgraph(newV).E;
 		return true;
 	}
 	
-	public boolean remove(int[] e) {
+	public boolean remove(int[] e) {  //Removes an edge e from this. Returns false if e is not in this.E
 		if(!(e.length == 2))
 			return false;
 		
@@ -133,11 +141,13 @@ public class SimpleGraph extends Graph {
 				return true;
 			}
 		}
+		
 		return false;
 	}
 	
-	public boolean add(int v) {
+	public boolean add(int v) {  //Adds a vertex v to this. Returns false if v is already in this.V
 		int[] newV = new int[this.V.length+1];
+		
 		for(int i = 0; i < this.V.length; i++) {
 			if(this.V[i] == v)
 				return false;
@@ -150,18 +160,20 @@ public class SimpleGraph extends Graph {
 		return true;
 	}
 	
-	public boolean add(int[] e) {
-		if(!(e.length == 2))
+	public boolean add(int[] e) {  //Adds an edge e to this
+		if(!(e.length == 2))  //Returns false if e has length not equal to 2
 			return false;
-		if(!(in(this.V, e[0]) && in(this.V, e[1])))
+		
+		if(!(in(this.V, e[0]) && in(this.V, e[1])))  //Or if the vertices joined by e are not in this.V
 			return false;
 		
 		int[] eSym = {e[1], e[0]};
 		
-		for (int[] edge : this.E) {
+		for (int[] edge : this.E) {  //Or if e is already in this.E
 			if(Arrays.equals(edge, e) || Arrays.equals(edge, eSym))
 				return false;
 		}
+		
 		this.E.add(e);
 		return true;
 	}
@@ -170,38 +182,41 @@ public class SimpleGraph extends Graph {
 		int[] e1 = {a, b};
 		int[] e2 = {b, a};
 		
-		if(in(this.E, e1) || in(this.E, e2))
+		if(in(this.E, e1) || in(this.E, e2))  //Checks the symmetric edge to satisfy defn of simple graph
 			return true;
+		
 		return false;
 	}
 	
-	public boolean hasPath(int a, int b) {
-		if(!(in(V, a) && in(V, b)))
+	public boolean hasPath(int a, int b) {  //Returns true if there is a path in this from a to b
+		if(!(in(V, a) && in(V, b)))  //Returns false if a, b are not both in this
 			return false;
 		
-		if(this.isAdj(a, b))
+		if(a == b)
+			return true;
+		
+		if(this.isAdj(a, b))  //Returns true if a and b are adjacent
 			return true;
 		
 		int[] withoutA = new int[V.length-1];
 		int j = 0;
-		for(int i = 0; i < V.length; i++) {
+		
+		for(int i = 0; i < V.length; i++) { //Gets list of all vertices in this other than a
 			if (V[i] != a) {
 				withoutA[j] = V[i];
 				j++;
 			}	
 		}
 		
-		for(int v : V) {
-			
+		for(int v : V) {  //Recursively checks if any of a's adjacent vertices have a path to b, not including a.
 			if(this.isAdj(a, v) && this.inducedSubgraph(withoutA).hasPath(v, b))
 				return true;
 		}
 		
 		return false;
-		
 	}
 	
-	public ArrayList<Integer> getPath(int a, int b) {
+	public ArrayList<Integer> getPath(int a, int b) {  //Returns a shortest path in this from a to b. If no path exists, returns an empty ArrayList
 		if(!(in(V, a) && in(V, b) && this.hasPath(a, b)))
 			return new ArrayList<Integer>();
 		
@@ -212,21 +227,28 @@ public class SimpleGraph extends Graph {
 			return path;
 		}
 		
-		ArrayList<ArrayList<Integer>> paths = new ArrayList<ArrayList<Integer>>();
+		if(a == b){
+			ArrayList<Integer> path = new ArrayList<Integer>();
+			path.add(a);
+			return path;
+		}	
 		
+		ArrayList<ArrayList<Integer>> paths = new ArrayList<ArrayList<Integer>>();  //Initializes list to store all paths
 		int[] withoutA = new int[V.length-1];
 		int j = 0;
-		for(int i = 0; i < V.length; i++) {
+		
+		for(int i = 0; i < V.length; i++) {  //Constructs list of all vertices except a
 			if (V[i] != a) {
 				withoutA[j] = V[i];
 				j++;
 			}	
 		}
 		
-		for(int v : withoutA) {
+		for(int v : withoutA) {  //For all of a's adjacent vertices v, recursively gets shortest path from v to b, not using a
 			if (this.isAdj(a, v)) {
 				ArrayList<Integer> vPath = this.inducedSubgraph(withoutA).getPath(v, b);
-				if (vPath.size() > 0) {
+				
+				if (vPath.size() > 0) {  //For each nonempty path (nonempty -> there exists a path from v to b) insert a at front to get a path from a to b
 					vPath.add(0, a);
 					paths.add(vPath);
 				}
@@ -235,18 +257,17 @@ public class SimpleGraph extends Graph {
 		
 		ArrayList<Integer> shortestPath = paths.get(0);
 		
-		for(ArrayList<Integer> path : paths) {
+		for(ArrayList<Integer> path : paths) { //Iterates through paths to findest shortest path
 			if (path.size() < shortestPath.size())
 				shortestPath = path;
 		}
 		
 		return shortestPath;
-		
-		
 	}
 	
-	public int [] getAdj(int a) {
+	public int [] getAdj(int a) { //Gets list of vertices adjacent to a
 		int i = 0;
+		
 		for (int v : V) {
 			if(this.isAdj(a, v))
 				i++;
@@ -254,6 +275,7 @@ public class SimpleGraph extends Graph {
 		
 		int [] adj = new int [i];
 		i = 0;
+		
 		for (int j = 0; j < V.length; j++) {
 			if (this.isAdj(a, V[j])) {
 				adj[i] = V[j];
@@ -262,19 +284,18 @@ public class SimpleGraph extends Graph {
 		}
 		
 		return adj;
-		
 	}
 	
 	public boolean isConnected() {
 		int[] marked = new int[V.length];
-		
-		marked[0] = 1;
+		marked[0] = 1;  //Marks an arbitrary vertex of this
 		boolean wasMarked = true;
-		while(wasMarked) {
+		
+		while(wasMarked) {  //If a vertex was marked in the last iteration
 			wasMarked = false;
-			for (int i = 0; i < marked.length; i++) {
+			for (int i = 0; i < marked.length; i++) {  //For every marked vertex v, mark every unmarked vertex adjacent to v
 				if(marked[i] == 1) {
-					marked[i] = 2;
+					marked[i] = 2;  //Sets the marking of v to 2 to denote that adjacent vertices of v have been marked
 					for(int j = 0; j < marked.length; j++) {
 						if(this.isAdj(V[i], V[j]) && marked[j] == 0) {
 							wasMarked = true;
@@ -285,26 +306,20 @@ public class SimpleGraph extends Graph {
 			}
 		}
 		
-		return !in(marked, 0);
-		
-		
-		
-		
-		
+		return !in(marked, 0);	//If there is still an unmarked vertex in this, then it is disconnected
 	}
 	
-	public static SimpleGraph getUndirected(DirectedGraph G) {
-		
+	public static SimpleGraph getUndirected(DirectedGraph G) {  //Constructs the corresponding simple graph from directed graph G
 		ArrayList<int[]> edges = new ArrayList<int[]>();
 		
 		for (int i = 0; i < G.E.size(); i++) {
 			int[] eSym = {G.E.get(i)[1], G.E.get(i)[0]};
+			
 			if(!in(edges, G.E.get(i)) && !in(edges, eSym))
 				edges.add(G.E.get(i));		
 		}
 		
-		return new SimpleGraph(G.V, edges);
-		
+		return new SimpleGraph(G.V, edges);	
 	}
 	
 	public static boolean in(ArrayList<int[]> edges, int[] e) {
@@ -313,6 +328,7 @@ public class SimpleGraph extends Graph {
 			if(Arrays.equals(edge, e))
 				return true;
 		}
+		
 		return false;
 	}
 	
@@ -321,17 +337,18 @@ public class SimpleGraph extends Graph {
 			if (a == x)
 				return true;
 		}
+		
 		return false;
 	}
 	
 	public static int[] arrayWithout(int[] array, int a) {
 		int[] withoutA = new int[array.length-1];
 		int j = 0;
+		
 		for(int i = 0; i < array.length; i++) {
 			if (array[i] != a) {
 				withoutA[j] = array[i];
 				j++;
-			
 			}
 		}	
 			
@@ -340,26 +357,5 @@ public class SimpleGraph extends Graph {
 	
 	public static void main(String[] args) {
 		
-		SimpleGraph K = new SimpleGraph(7);
-		K.add(new int[] {0,1});
-		K.add(new int[] {0,2});
-		K.add(new int[] {1,3});
-		K.add(new int[] {1,4});
-		K.add(new int[] {2,5});
-		K.add(new int[] {2,6});
-		
-		System.out.println(K.isConnected());
-		K.remove(new int[] {0,1});
-		System.out.println(K.isConnected());
-		
-		SimpleGraph G = K(5);
-		System.out.println(G.isConnected());
-		G.add(7);
-		System.out.println(G.isConnected());
-		
-		
-		
-		
-	
 	}	
 }
